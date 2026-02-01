1966c061a8f6fed77e4e840588ca24ac1a60596a
birajjk112 <birajjk112@gmail.com>
2026-02-01 22:12:22 +0545

    Adding item add feature

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@ import { createItems } from "./items.js";
 
 let items = groceryItems;
 
-// Render App
 function render() {
   const app = document.getElementById("app");
   app.innerHTML = "";
 
+  const formElement = createForm();
   const itemsElement = createItems(items);
+
+  app.appendChild(formElement);
   app.appendChild(itemsElement);
 }
 
@@ -35,4 +37,23 @@ export function removeItem(itemId) {
   items = items.filter((item) => item.id !== itemId);
   render();
   setTimeout(() => alert("Item Deleted Successfully!"), 0);
+}
+// ....
+import { createForm } from "./form.js";
+
+// Generate unique ID
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+// Add Item Function
+export function addItem(itemName) {
+  const newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items = [...items, newItem];
+  render();
+  setTimeout(() => alert("Item Added Successfully!"), 0);
 }
